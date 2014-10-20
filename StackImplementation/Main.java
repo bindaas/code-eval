4c27bc357a6d6ea5e09375569317446be57b53c3
bindaas <rajiv.narula@gmail.com>
2014-10-20 11:37:52 -0400

    Stack Implementation

--- StackImplementation/Main.java
+++ StackImplementation/Main.java
@@ -1,6 +1,7 @@
 import java.io.* ;
 
 public class Main {
+
     public static void main (String[] args) throws Exception{
     File file = new File(args[0]);
     BufferedReader in = new BufferedReader(new FileReader(file));
@@ -8,9 +9,24 @@ public class Main {
     while ((line = in.readLine()) != null) {
         String[] lineArray = line.split(" ");
         if (lineArray.length > 0) {
-			System.out.println (line);
+			doublePop (lineArray);
+			System.out.println ();
         }
     }
-  }
+	}
 
-  }
+    private static void doublePop (String[] stack){
+		int size = stack.length ;
+		boolean hasElements = size >=2 ;
+		int i = size-1;
+		while (hasElements){
+			System.out.print (stack[i]+" ");
+			i = i -2 ;
+			hasElements = i >=0 ;
+		}
+
+	}
+
+
+
+}
