f8d758db2537200bab129f6b6d289c119cc9f1fa
bindaas <rajiv.narula@gmail.com>
2014-10-19 22:30:22 -0400

    FizzBizz completed

--- FizzBizz/Main.java
+++ FizzBizz/Main.java
@@ -8,16 +8,40 @@ public class Main {
     while ((line = in.readLine()) != null) {
         String[] lineArray = line.split(" ");
         if (lineArray.length > 0) {
-            Integer divider1 = new Integer(lineArray[0]);
-            Integer divider2 = new Integer(lineArray[1]);
-            Integer count = new Integer(lineArray[2]);
+            int divider1 = new Integer(lineArray[0]).intValue();
+            int divider2 = new Integer(lineArray[1]).intValue();
+            int count = new Integer(lineArray[2]).intValue();
 			fizzBizz (divider1 , divider2 , count);
+			System.out.println ();
         }
     }
   }
 
-  private static void fizzBizz (Integer divider1 , Integer divider2 , Integer count){
-		System.out.println ("divider1->"+divider1+"\tdivider2->"+divider2+"\tcount->"+count);
+  private static void fizzBizz (int divider1 , int divider2 , int count){
+		for (int i= 1 ; i <= count;i++){
+			boolean isFizz = false;
+			boolean isBizz = false;
+
+			if (i%divider1==0){
+				isFizz = true ;
+			}
+			if (i%divider2==0){
+				isBizz = true ;
+			}
+			if (isFizz){
+				System.out.print ("F");
+			}
+			if (isBizz){
+				System.out.print ("B");
+			}
+			if ((!isFizz)&& (!isBizz)){
+				System.out.print (i);
+			}
+			if (i != count){
+				System.out.print (" ");
+			}
+
+		}
 
   }
 
