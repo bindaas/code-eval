ca80199ff2a5408e25287b072fe3cee5dc76299a
bindaas <rajiv.narula@gmail.com>
2014-10-22 13:53:09 -0400

    TextDollar

--- TextDollar/Main.java
+++ TextDollar/Main.java
@@ -8,9 +8,29 @@ public class Main {
     while ((line = in.readLine()) != null) {
         String[] lineArray = line.split(" ");
         if (lineArray.length > 0) {
-			System.out.println(line);
+			System.out.println(convertIntoTextDollars(line));
         }
     }
   }
 
+  private static String convertIntoTextDollars (String amount){
+  	String convertedText ="Dollars";
+  	for (int i = amount.length()-1 ; i >=0; --i){
+		char aChar = amount.charAt(i);
+		String digit=convertDigit (aChar, i);
+		convertedText = digit +convertedText ;
+
+	}
+
+  	return convertedText;
   }
+
+
+  private static String convertDigit (char c , int place){
+	  if (c =='1'){
+		  return "One";
+      }
+  	return "Zero";
+  }
+
+}
