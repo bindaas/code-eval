ad5782eecf31f65ac3d641cabc0f43344bf32303
bindaas <rajiv.narula@gmail.com>
2014-10-26 21:48:49 -0400

    UglyNumbers

---
+++ UglyNumbers/Main.java
@@ -0,0 +1,15 @@
+import java.io.* ;
+
+public class Main {
+    public static void main (String[] args) throws Exception{
+    File file = new File(args[0]);
+    BufferedReader in = new BufferedReader(new FileReader(file));
+    String line;
+    while ((line = in.readLine()) != null) {
+        String[] lineArray = line.split(" ");
+        if (lineArray.length > 0) {
+			System.out.println("line->"+line);
+        }
+    }
+  }
+}
