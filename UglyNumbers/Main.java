1d8a5bcc53a7912cd88502027d7dd3f2bc5ea793
bindaas <rajiv.narula@gmail.com>
2014-10-30 21:31:29 -0400

    Ugly Numbers

--- UglyNumbers/Main.java
+++ UglyNumbers/Main.java
@@ -1,4 +1,5 @@
 import java.io.* ;
+import java.util.* ;
 
 public class Main {
     public static void main (String[] args) throws Exception{
@@ -8,8 +9,39 @@ public class Main {
     while ((line = in.readLine()) != null) {
         String[] lineArray = line.split(" ");
         if (lineArray.length > 0) {
-			System.out.println("line->"+line);
+			System.out.println("line->"+line+" countUglyNumbers:"+countUglyNumbers(line));
         }
     }
   }
+
+
+  private static int countUglyNumbers(String number){
+	int count = 0;
+	List<String> expressions = generateExpressions (number);
+	for (String expression:expressions){
+		int value = calculateExpression (expression);
+		if (isUglyNumber(value)){
+			count++;
+		}
+	}
+	return count ;
+  }
+
+
+  private static List <String> generateExpressions (String number){
+  	return new ArrayList<String> (0);
+  }
+
+  private static int calculateExpression (String expression){
+  	return 0 ;
+  }
+
+  private static boolean isUglyNumber( int number){
+	  if ((number % 2) == 0) return true;
+	  else if ((number % 3) == 0) return true;
+	  else if ((number % 5) == 0) return true;
+	  else if ((number % 7) == 0) return true;
+  	  else return false ;
+  }
+
 }
