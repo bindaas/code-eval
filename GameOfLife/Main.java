7136b5b564d6b6b35fb2159cef6ac8075d755ab5
bindaas <rajiv.narula@gmail.com>
2015-04-19 23:20:32 -0400

    GameOfLife
    Basic structure

---
+++ GameOfLife/Main.java
@@ -0,0 +1,49 @@
+import java.io.* ;
+import java.util.* ;
+
+public class Main {
+    public static void main (String[] args) throws Exception{
+    File file = new File(args[0]);
+    BufferedReader in = new BufferedReader(new FileReader(file));
+    String line;
+    List <List<Boolean>> matrix = new ArrayList <List<Boolean>>();
+    while ((line = in.readLine()) != null) {
+			System.out.println (line);
+			List<Boolean>cellsRow = parseCells(line);
+			matrix.add (cellsRow);
+    }
+
+  }
+
+  private static List<Boolean>  parseCells (String line){
+		List<Boolean> cells = new ArrayList<Boolean> (100);
+		char[] charArray = line.toCharArray();
+		for (char c :charArray){
+			Boolean isAlive = (c=='*')?new Boolean (true):new Boolean (false);
+			cells.add (isAlive);
+		}
+		return cells ;
+  }
+
+
+   private void runGenerations(List <List<Boolean>> matrix, int count){
+
+   }
+
+   private void printMatrix (List <List<Boolean>> matrix){
+
+   }
+
+   private List <List<Boolean>> transformMatrix (List <List<Boolean>> matrix){
+   		return null ;
+   }
+
+  private List<Boolean> getNeighbours (int row,int col,List <List<Boolean>> matrix){
+	  return null ;
+  }
+
+  private Boolean transformCell (Boolean currentState,List<Boolean> neighbours){
+  	return null ;
+  }
+
+}
